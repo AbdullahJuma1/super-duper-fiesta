74a29f0a627fb88f3a4cf215699905107cabb553
AbdullahJuma1 <63639017+AbdullahJuma1@users.noreply.github.com>
2021-10-24 23:30:37 +0300

    New updates of the files

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        
+
+      $this->validate($request, [
+          'name' => 'required|max:255',
+          'username' => 'required|max:255',
+          'email' => 'required|email|max:255',
+          //'phonenumber' => 'required|max:255',
+          'password' => 'required|confirmed',
+      ]);
+
+     User::create([
+
+        'name' => $request->name,
+        'username' => $request->username,
+        'email' => $request->email,
+        //'phonenumber' => $request->phonenumber,
+        'password' => Hash::make($request->password),
+     ]);
+
+     //sign in
+
+       auth()->attempt($request->only('email', 'password'));
+
+     return redirect()->route('dashboard');
+
+
+        //validation
+        //store user
+        //sign the user in
+        //redirect
+    }
+}
