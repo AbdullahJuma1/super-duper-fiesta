77020c2b64b17a4b109d90dd2388fc2636255b96
AbdullahJuma1 <63639017+AbdullahJuma1@users.noreply.github.com>
2021-10-28 01:39:21 +0300

    In this update, I managed to create a post form for users to post on to the site. and connected the post form with the database. then created a relationship between the post and the users. Also touched a bit on pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,17 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts/index');
+        $posts = Post::paginate(10);
+
+      
+
+        return view('posts/index',
+    [
+        'posts' => $posts
+    ]);
     }
 
     public function store(Request $request)
     {
-        dd('okie dokie');
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+       $request->user()->posts()->create($request->only('body'));
+
+       return back();
     }
 }
